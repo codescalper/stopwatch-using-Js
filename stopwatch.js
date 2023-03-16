5e8cfbabb576b3a19828d5a9da5ee69db2ba18d6
codescalper <msfunbook@gmail.com>
2023-03-16 22:47:27 +0530

    Initialized variables

---
+++ stopwatch.js
@@ -0,0 +1,19 @@
+window.onload = function(){
+    let minutes = 0;
+    let seconds = 0;
+    let tens = 00;
+
+    let appendMinutes = document.querySelector('#minutes');
+    let appendSecond = document.querySelector('#seconds');
+    let appendTens = document.querySelector('#tens');
+
+    let startBtn = document.querySelector('#start');
+    let stopBtn = document.querySelector('#stop');
+    let resetBtn = document.querySelector('#reset');
+
+
+    const startTimer = ()=>{
+        
+    }
+
+}
